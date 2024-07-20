ad9d68d8db7d490cd54fb76d27d6e46651a9c8e1
dm1cael <167719716+dm1cael@users.noreply.github.com>
2024-07-20 19:06:39 -0300

    add operate function

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return sub(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            return 'Unknown operation';
+    }
+}
+
+// Basic functions
+
 function add(a, b) {
     return a + b;
 }
